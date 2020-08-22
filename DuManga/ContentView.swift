90ee8fb1ae32debecdb87c9efda659cfd8a16a69
doraemoe <jinyifan@outlook.com>
2020-08-22 20:56:55 +1000

    Initial Commit

---
+++ DuManga/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DuManga
+//
+//  Created by Jin Yifan on 22/8/20.
+//  Copyright © 2020 Jin Yifan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
