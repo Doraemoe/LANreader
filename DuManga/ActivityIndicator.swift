ddf7939a2baf9e19d51aefe67d302747bf386289
Doraemoe <jinyifan@outlook.com>
2020-08-26 22:09:19 +1000

    add loading indecator (#7)

---
+++ DuManga/ActivityIndicator.swift
@@ -0,0 +1,19 @@
+//Created 26/8/20
+
+// TODO: Replace it with ProgressView in future Xcode 12
+
+import SwiftUI
+
+struct ActivityIndicator: UIViewRepresentable {
+
+    @Binding var isAnimating: Bool
+    let style: UIActivityIndicatorView.Style
+
+    func makeUIView(context: UIViewRepresentableContext<ActivityIndicator>) -> UIActivityIndicatorView {
+        return UIActivityIndicatorView(style: style)
+    }
+
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: UIViewRepresentableContext<ActivityIndicator>) {
+        isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
+    }
+}
