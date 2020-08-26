8c3af55fedbd9907b1a2a6cde596d607f52473cd
doraemoe <jinyifan@outlook.com>
2020-08-26 21:49:18 +1000

    add tabview for future function

--- DuManga/ContentView.swift
+++ DuManga/ContentView.swift
@@ -4,15 +4,30 @@ import SwiftUI
 
 struct ContentView: View {
     @State var settingView = UserDefaults.standard.dictionary(forKey: "LANraragi") == nil
+    @State var navBarTitle: String = ""
     
     var body: some View {
-            VStack(alignment: .leading) {
-                if (self.settingView) {
-                    LANraragiConfigView(settingView: $settingView)
-                } else {
-                    ArchiveList(settingView: $settingView)
+        VStack(alignment: .leading) {
+            if (self.settingView) {
+                LANraragiConfigView(settingView: $settingView)
+            } else {
+                NavigationView {
+                    TabView{
+                        ArchiveList(navBarTitle: $navBarTitle)
+                            .tabItem {
+                                Text("library")
+                        }
+                    }
+                    .navigationBarTitle(Text(navBarTitle), displayMode: .inline)
+                    .navigationBarItems(trailing:Button(action: {
+                        self.settingView.toggle()
+                    }) {
+                        Text("settings")
+                    } )
                 }
+                .navigationViewStyle(StackNavigationViewStyle())
             }
+        }
     }
 }
 
