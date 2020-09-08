a6c8dfb952a808ebfa4177c3fd91059d34c6efd6
Doraemoe <jinyifan@outlook.com>
2020-09-08 19:54:26 +1000

    better edit category experience (#35)

--- DuManga/CategoryList.swift
+++ DuManga/CategoryList.swift
@@ -1,81 +1,92 @@
 //Created 27/8/20
 
 import SwiftUI
+import NotificationBannerSwift
 
 struct CategoryList: View {
-    @State var categoryItems = [CategoryItem]()
+    @State var categoryItems = [String: CategoryItem]()
     @State var isLoading = false
     @State var showSheetView = false
     @State var selectedCategoryItem: CategoryItem? = nil
-    
+
     @Binding var navBarTitle: String
     @Binding var editMode: EditMode
-    
+
     private let config: [String: String]
     private let client: LANRaragiClient
-    
+
     init(navBarTitle: Binding<String>, editMode: Binding<EditMode>) {
         self.config = UserDefaults.standard.dictionary(forKey: "LANraragi") as? [String: String] ?? [String: String]()
         self.client = LANRaragiClient(url: config["url"]!, apiKey: config["apiKey"]!)
         self._navBarTitle = navBarTitle
         self._editMode = editMode
     }
-    
+
     var body: some View {
-        GeometryReader { geometry in
+        let categories = Array(self.categoryItems.values).sorted(by: { $0.name < $1.name })
+        return GeometryReader { geometry in
             ZStack {
-                List(self.categoryItems) { (item: CategoryItem) in
+                List(categories) { (item: CategoryItem) in
                     if self.editMode == .active {
                         ZStack(alignment: .leading) {
                             Text(item.name)
-                                .font(.title)
+                                    .font(.title)
                             Rectangle()
-                                .opacity(0.0001)
-                                .contentShape(Rectangle())
-                                .onTapGesture(perform: {
-                                    self.selectedCategoryItem = item
-                                    self.showSheetView = true
-                                })
+                                    .opacity(0.0001)
+                                    .contentShape(Rectangle())
+                                    .onTapGesture(perform: {
+                                        self.selectedCategoryItem = item
+                                        self.showSheetView = true
+                                    })
                         }
                     } else {
-                        NavigationLink(destination: ArchiveList(navBarTitle: self.$navBarTitle, searchKeyword: item.search, categoryArchives: item.archives, navBarTitleOverride: "category")) {
+                        NavigationLink(destination: ArchiveList(navBarTitle: self.$navBarTitle,
+                                searchKeyword: item.search,
+                                categoryArchives: item.archives,
+                                navBarTitleOverride: "category")) {
                             Text(item.name)
-                                .font(.title)
+                                    .font(.title)
                         }
                     }
                 }
-                .sheet(isPresented: self.$showSheetView) {
-                    EditCategory(item: self.selectedCategoryItem!, showSheetView: self.$showSheetView)
-                        .onDisappear(perform: {
-                            self.categoryItems = [CategoryItem]()
-                            self.loadData()
-                        })
-                }
-                .onAppear(perform: { self.navBarTitle = "category" })
-                .onAppear(perform: self.loadData)
-                
+                        .sheet(isPresented: self.$showSheetView) {
+                            EditCategory(item: self.selectedCategoryItem!,
+                                    showSheetView: self.$showSheetView,
+                                    categoryItems: self.$categoryItems)
+                        }
+                        .onAppear(perform: { self.navBarTitle = "category" })
+                        .onAppear(perform: self.loadData)
+
                 VStack {
                     Text("loading")
                     ActivityIndicator(isAnimating: self.$isLoading, style: .large)
                 }
-                .frame(width: geometry.size.width / 3,
-                       height: geometry.size.height / 5)
-                    .background(Color.secondary.colorInvert())
-                    .foregroundColor(Color.primary)
-                    .cornerRadius(20)
-                    .opacity(self.isLoading ? 1 : 0)
+                        .frame(width: geometry.size.width / 3,
+                                height: geometry.size.height / 5)
+                        .background(Color.secondary.colorInvert())
+                        .foregroundColor(Color.primary)
+                        .cornerRadius(20)
+                        .opacity(self.isLoading ? 1 : 0)
             }
         }
     }
-    
+
     func loadData() {
         if self.categoryItems.count > 0 {
             return
         }
         self.isLoading = true
         client.getCategories { (items: [ArchiveCategoriesResponse]?) in
+            if items == nil {
+                let banner = NotificationBanner(title: NSLocalizedString("error", comment: "error"),
+                        subtitle: NSLocalizedString("error.category", comment: "category error"), style: .danger)
+                banner.show()
+            }
             items?.forEach { item in
-                self.categoryItems.append(CategoryItem(id: item.id, name: item.name, archives: item.archives, search: item.search, pinned: item.pinned))
+                if self.categoryItems[item.id] == nil {
+                    self.categoryItems[item.id] = CategoryItem(id: item.id, name: item.name,
+                            archives: item.archives, search: item.search, pinned: item.pinned)
+                }
             }
             self.isLoading = false
         }
--- DuManga/EditCategory.swift
+++ DuManga/EditCategory.swift
@@ -4,23 +4,25 @@ import SwiftUI
 import NotificationBannerSwift
 
 struct EditCategory: View {
-    
+
     @State var categoryName = ""
     @State var searchKeyword = ""
-    
+
     @Binding var showSheetView: Bool
-    
+    @Binding var categoryItems: [String: CategoryItem]
+
     let item: CategoryItem
     private let config: [String: String]
     private let client: LANRaragiClient
-    
-    init(item: CategoryItem, showSheetView: Binding<Bool>) {
+
+    init(item: CategoryItem, showSheetView: Binding<Bool>, categoryItems: Binding<[String: CategoryItem]>) {
         self.item = item
         self.config = UserDefaults.standard.dictionary(forKey: "LANraragi") as? [String: String] ?? [String: String]()
         self.client = LANRaragiClient(url: config["url"]!, apiKey: config["apiKey"]!)
         self._showSheetView = showSheetView
+        self._categoryItems = categoryItems
     }
-    
+
     var body: some View {
         NavigationView {
             VStack {
@@ -28,53 +30,54 @@ struct EditCategory: View {
                     VStack(alignment: .leading) {
                         Text("category.name")
                         TextField("name", text: self.$categoryName)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
                     }
-                    .padding()
-                    .onAppear(perform: {
-                        self.categoryName = self.item.name
-                        self.searchKeyword = self.item.search
-                    })
+                            .padding()
+                            .onAppear(perform: {
+                                self.categoryName = self.item.name
+                                self.searchKeyword = self.item.search
+                            })
                     VStack(alignment: .leading) {
                         Text("category.search")
                         TextField("search", text: self.$searchKeyword)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
                     }
-                    .padding()
+                            .padding()
                 } else {
                     Text("Only support dynamic category edit for now.")
                 }
-            }  
-            .navigationBarTitle("category.edit", displayMode: .inline)
-            .navigationBarItems(leading: Button(action: {
-                self.showSheetView = false
-            }) {
-                Text("cancel")
-            }, trailing: Button(action: {
-                let updated: CategoryItem = CategoryItem(id: self.item.id, name: self.categoryName, archives: [], search: self.searchKeyword, pinned: self.item.pinned)
-                self.client.updateSearchCategory(item: updated) { success in
-                    if success {
+            }
+                    .navigationBarTitle("category.edit", displayMode: .inline)
+                    .navigationBarItems(leading: Button(action: {
                         self.showSheetView = false
-                    } else {
-                        let banner = NotificationBanner(title: NSLocalizedString("error", comment: "error"), subtitle: NSLocalizedString("error.category.update", comment: "update category error"), style: .danger)
-                        banner.show()
-                    }
-                }
-                
-            }) {
-                Text("done")
-            })
+                    }) {
+                        Text("cancel")
+                    }, trailing: Button(action: {
+                        let updated: CategoryItem = CategoryItem(id: self.item.id,
+                                name: self.categoryName, archives: [], search: self.searchKeyword, pinned: self.item.pinned)
+                        self.client.updateSearchCategory(item: updated) { success in
+                            if success {
+                                self.categoryItems[updated.id] = updated
+                                self.showSheetView = false
+                            } else {
+                                let banner = NotificationBanner(title: NSLocalizedString("error", comment: "error"),
+                                        subtitle: NSLocalizedString("error.category.update", comment: "update category error"),
+                                        style: .danger)
+                                banner.show()
+                            }
+                        }
+
+                    }) {
+                        Text("done")
+                    })
         }
-        .navigationViewStyle(StackNavigationViewStyle())
-    }
-    
-    func saveSearchCategory() {
-        return
+                .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
 struct EditCategory_Previews: PreviewProvider {
     static var previews: some View {
-        EditCategory(item: CategoryItem(id: "id", name: "name", archives: [], search: "search", pinned: "0"), showSheetView: Binding.constant(true))
+        EditCategory(item: CategoryItem(id: "id", name: "name", archives: [], search: "search", pinned: "0"),
+                showSheetView: Binding.constant(true), categoryItems: Binding.constant([String: CategoryItem]()))
     }
 }
