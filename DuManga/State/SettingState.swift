f01adfb43954182d7adb80f809aa7d77e20c8257
Doraemoe <jinyifan@outlook.com>
2020-09-21 20:58:15 +1000

    cleanup settings store (#48)

--- DuManga/State/SettingState.swift
+++ DuManga/State/SettingState.swift
@@ -9,15 +9,6 @@ struct SettingState {
     var apiKey: String
     var errorCode: ErrorCode?
     var savedSuccess: Bool
-    var tapLeft: PageControl
-    var tapMiddle: PageControl
-    var tapRight: PageControl
-    var swipeLeft: PageControl
-    var swipeRight: PageControl
-    var splitPage: Bool
-    var splitPagePriorityLeft: Bool
-    var archiveListRandom: Bool
-    var useListView: Bool
 
     init() {
         // server
@@ -25,23 +16,5 @@ struct SettingState {
         self.apiKey = UserDefaults.standard.string(forKey: SettingsKey.lanraragiApiKey) ?? ""
         self.errorCode = nil
         self.savedSuccess = false
-        // tap
-        self.tapLeft = PageControl(rawValue: UserDefaults.standard.string(forKey: SettingsKey.tapLeftKey)
-                ?? PageControl.next.rawValue)!
-        self.tapMiddle = PageControl(rawValue: UserDefaults.standard.string(forKey: SettingsKey.tapMiddleKey)
-                ?? PageControl.navigation.rawValue)!
-        self.tapRight = PageControl(rawValue: UserDefaults.standard.string(forKey: SettingsKey.tapRightKey)
-                ?? PageControl.previous.rawValue)!
-        // swipe
-        self.swipeLeft = PageControl(rawValue: UserDefaults.standard.string(forKey: SettingsKey.swipeLeftKey)
-                ?? PageControl.next.rawValue)!
-        self.swipeRight = PageControl(rawValue: UserDefaults.standard.string(forKey: SettingsKey.swipeRightKey)
-                ?? PageControl.previous.rawValue)!
-        // split
-        self.splitPage = UserDefaults.standard.bool(forKey: SettingsKey.splitPage)
-        self.splitPagePriorityLeft = UserDefaults.standard.bool(forKey: SettingsKey.splitPagePriorityLeft)
-        // view
-        self.archiveListRandom = UserDefaults.standard.bool(forKey: SettingsKey.archiveListRandom)
-        self.useListView = UserDefaults.standard.bool(forKey: SettingsKey.useListView)
     }
 }
