fea780d226147a34e4db96de7252667e28ecc3e8
Doraemoe <jinyifan@outlook.com>
2020-09-27 00:14:11 +1000

    use native v grid in ios14 (#52)

--- DuManga/ArchiveGrid.swift
+++ DuManga/ArchiveGrid.swift
@@ -8,24 +8,25 @@ struct ArchiveGrid: View {
     var body: some View {
         VStack(alignment: HorizontalAlignment.center, spacing: 2) {
             Text(archiveItem.name)
-                .frame(width: 150)
+                    .frame(width: 150)
+                    .lineLimit(1)
             archiveItem.thumbnail
-            .resizable()
-            .scaledToFit()
-            .frame(width: 180, height: 225)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 180, height: 225)
         }
-        .overlay(
-            RoundedRectangle(cornerRadius: 8)
-                .stroke(Color.secondary, lineWidth: 2)
-                .opacity(0.9)
-                .frame(width: 180, height: 260, alignment: .center)
-        )
+                .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.secondary, lineWidth: 2)
+                                .opacity(0.9)
+                                .frame(width: 180, height: 260, alignment: .center)
+                )
     }
 }
 
 struct ArchiveGrid_Previews: PreviewProvider {
     static var previews: some View {
         ArchiveGrid(archiveItem: ArchiveItem(id: "id", name: "name", tags: "tags", thumbnail: Image("placeholder")))
-        .previewLayout(.fixed(width: 200, height: 280))
+                .previewLayout(.fixed(width: 200, height: 280))
     }
 }
