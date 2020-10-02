3563decdb9e61176caae8c72c6acb42713868311
doraemoe <jinyifan@outlook.com>
2020-10-02 23:08:16 +1000

    compatibility change to setting view

--- DuManga/LANraragiConfigView.swift
+++ DuManga/LANraragiConfigView.swift
@@ -7,32 +7,21 @@ struct LANraragiConfigView: View {
     @EnvironmentObject var store: AppStore
     @Environment(\.presentationMode) var presentationMode
 
-    @State var url = ""
-    @State var apiKey = ""
+    @StateObject var configModel = LANraragiConfigViewModel()
 
     var body: some View {
-        if store.state.setting.errorCode != nil {
-            let banner = NotificationBanner(title: NSLocalizedString("error", comment: "error"),
-                    subtitle: NSLocalizedString("error.host", comment: "host error"),
-                    style: .danger)
-            banner.show()
-            self.store.dispatch(.setting(action: .resetState))
-        } else if store.state.setting.savedSuccess {
-            self.store.dispatch(.setting(action: .resetState))
-            self.presentationMode.wrappedValue.dismiss()
-        }
-
-        return VStack {
-            TextField("lanraragi.config.url", text: self.$url)
+        VStack {
+            TextField("lanraragi.config.url", text: self.$configModel.url)
                     .textContentType(.URL)
                     .keyboardType(.URL)
                     .padding()
                     .textFieldStyle(RoundedBorderTextFieldStyle())
-            SecureField("lanraragi.config.apiKey", text: self.$apiKey)
+            SecureField("lanraragi.config.apiKey", text: self.$configModel.apiKey)
                     .padding()
                     .textFieldStyle(RoundedBorderTextFieldStyle())
             Button(action: {
-                self.store.dispatch(.setting(action: .verifyAndSaveLanraragiConfig(url: self.url, apiKey: self.apiKey)))
+                self.store.dispatch(.setting(action: .verifyAndSaveLanraragiConfig(
+                        url: self.configModel.url, apiKey: self.configModel.apiKey)))
             }, label: {
                 Text("lanraragi.config.submit")
                         .font(.headline)
@@ -40,8 +29,25 @@ struct LANraragiConfigView: View {
                     .padding()
         }
         .onAppear(perform: {
-            self.url = self.store.state.setting.url
-            self.apiKey = self.store.state.setting.apiKey
+            self.configModel.load(state: store.state)
+        })
+        .onDisappear(perform: {
+            self.configModel.unload()
+        })
+        .onChange(of: self.configModel.errorCode, perform: { errorCode in
+            if errorCode != nil {
+                let banner = NotificationBanner(title: NSLocalizedString("error", comment: "error"),
+                        subtitle: NSLocalizedString("error.host", comment: "host error"),
+                        style: .danger)
+                banner.show()
+                self.store.dispatch(.setting(action: .resetState))
+            }
+        })
+        .onChange(of: self.configModel.savedSuccess, perform: { success in
+            if success {
+                self.store.dispatch(.setting(action: .resetState))
+                self.presentationMode.wrappedValue.dismiss()
+            }
         })
     }
 }
