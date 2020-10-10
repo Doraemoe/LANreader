ca3a2875d0e926477e50fa1eb4cba828338ec64c
Doraemoe <jinyifan@outlook.com>
2020-10-10 13:56:29 +1000

    rearrange files (#63)

--- DuManga/Models/AppModels.swift
+++ DuManga/Models/AppModels.swift
@@ -4,6 +4,28 @@
 
 import Foundation
 
+enum PageControl: String, CaseIterable, Identifiable {
+    case next
+    case previous
+    case navigation
+
+    var id: String { self.rawValue }
+}
+
+struct SettingsKey {
+    static let lanraragiUrl = "settings.lanraragi.url"
+    static let lanraragiApiKey = "settings.lanraragi.apiKey"
+    static let tapLeftKey = "settings.read.tap.left"
+    static let tapMiddleKey = "settings.read.tap.middle"
+    static let tapRightKey = "settings.read.tap.right"
+    static let swipeLeftKey = "settings.read.swipe.left"
+    static let swipeRightKey = "settings.read.swipe.right"
+    static let splitPage = "settings.read.split.page"
+    static let splitPagePriorityLeft = "settings.read.split.page.priority.left"
+    static let archiveListRandom = "settings.archive.list.random"
+    static let useListView = "settings.view.use.list"
+}
+
 struct ErrorCode: Equatable {
 
     static func == (lhs: ErrorCode, rhs: ErrorCode) -> Bool {
