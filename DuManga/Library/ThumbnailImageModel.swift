3977a57241931b7c2c0f61665ca94fdc5aa9c558
Doraemoe <jinyifan@outlook.com>
2021-04-14 22:50:22 +1000

    Should not use StateObject init (#97)

--- DuManga/Library/ThumbnailImageModel.swift
+++ DuManga/Library/ThumbnailImageModel.swift
@@ -13,25 +13,24 @@ class ThumbnailImageModel: ObservableObject {
 
     private(set) var isLoading = false
 
-    private let id: String
     private let service = LANraragiService.shared
     private let database = AppDatabase.shared
     private var cancellable: Set<AnyCancellable> = []
-
-    init(id: String) {
-        self.id = id
-    }
 
     deinit {
         unload()
     }
 
-    func load() {
-        guard !isLoading else { return }
-        guard image == Image("placeholder") else { return }
+    func load(id: String) {
+        guard !isLoading else {
+            return
+        }
+        guard image == Image("placeholder") else {
+            return
+        }
         do {
             if let archive = try database.readArchive(id) {
-                self.image = Image(uiImage: UIImage(data: archive.thumbnail)!)
+                image = Image(uiImage: UIImage(data: archive.thumbnail)!)
                 return
             }
         } catch {
@@ -39,32 +38,32 @@ class ThumbnailImageModel: ObservableObject {
         }
 
         service.retrieveArchiveThumbnailData(id: id)
-            .subscribe(on: Self.imageProcessingQueue)
-            .handleEvents(receiveSubscription: {[weak self] _ in self?.onStart()},
-                          receiveCompletion: {[weak self] _ in self?.onFinish() },
-                          receiveCancel: {[weak self] in self?.onFinish() })
-            .receive(on: DispatchQueue.main)
-            .sink(receiveCompletion: { completion in
-                switch completion {
-                case .failure(let error):
-                    ThumbnailImageModel.logger.error("Error \(error)")
-                case .finished:
-                    return
-                }
-            }, receiveValue: {
-                self.image = Image(uiImage: UIImage(data: $0)!)
-                var archive = Archive(id: self.id, thumbnail: $0, lastUpdate: Date())
-                do {
-                    try self.database.saveArchive(&archive)
-                } catch {
-                    ThumbnailImageModel.logger.error("db error. id=\(self.id) \(error)")
-                }
-            })
-            .store(in: &cancellable)
+                .subscribe(on: Self.imageProcessingQueue)
+                .handleEvents(receiveSubscription: { [weak self] _ in self?.onStart() },
+                        receiveCompletion: { [weak self] _ in self?.onFinish() },
+                        receiveCancel: { [weak self] in self?.onFinish() })
+                .receive(on: DispatchQueue.main)
+                .sink(receiveCompletion: { completion in
+                    switch completion {
+                    case .failure(let error):
+                        ThumbnailImageModel.logger.error("Error \(error)")
+                    case .finished:
+                        return
+                    }
+                }, receiveValue: {
+                    self.image = Image(uiImage: UIImage(data: $0)!)
+                    var archive = Archive(id: id, thumbnail: $0, lastUpdate: Date())
+                    do {
+                        try self.database.saveArchive(&archive)
+                    } catch {
+                        ThumbnailImageModel.logger.error("db error. id=\(id) \(error)")
+                    }
+                })
+                .store(in: &cancellable)
     }
 
     func unload() {
-        self.onFinish()
+        onFinish()
         cancellable.forEach({ $0.cancel() })
     }
 
